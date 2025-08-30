469c1b08ce2cb875611acf0e9581eeb8330e8341
eiwashita <erikoiwashita@gmail.com>
2025-08-29 19:22:37 -0600

    Add components
    - Add cart buttons component
    - Add shopping cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 // import { allProducts } from './assets/data';
 import CartItem from './components/CartItem';
+import ShoppingCart from './components/ShoppingCart';
 import { useCart } from './context/CartContext';
 
 const App = () => {
@@ -10,11 +11,16 @@ const App = () => {
     setItems();
   }, []);
 
+  useEffect(() => {
+    console.log(allItems);
+  }, [allItems]);
+
   return (
     <div className="grid place-items-center py-20">
-      <h1 className="text-5xl italic text-gray-500 mb-16">
+      <h1 className="text-5xl italic text-gray-500 mb-16 font-sans">
         Trend Alert: Must-Have Outfits of the Season
       </h1>
+      <ShoppingCart />
       <div className="grid grid-cols-3 place-items-start gap-10">
         {allItems?.map((item) => {
           return <CartItem key={item.id} item={item} />;
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -12,7 +12,41 @@ export const CartProvider = ({ children }) => {
     setAllItems(allProducts);
   };
 
-  return <CartContext.Provider value={{ allItems, setItems }}>{children}</CartContext.Provider>;
+  const addToCart = (item) => {
+    setAllItems((prevItems) => {
+      return prevItems.map((prevItem) => {
+        if (prevItem.inCart) {
+          return prevItem;
+        }
+
+        return prevItem.id === item.id ? { ...prevItem, inCart: true } : prevItem;
+      });
+    });
+  };
+
+  const removeFromCart = (item) => {
+    setAllItems((prevItems) => {
+      return prevItems.map((prevItem) => {
+        return prevItem.id === item.id ? { ...prevItem, inCart: false, quantity: 1 } : prevItem;
+      });
+    });
+  };
+
+  const updateQuantity = (cartItem, amount) => {
+    setAllItems((prevItems) => {
+      return prevItems.map((prevItem) => {
+        return prevItem.id === cartItem.id
+          ? { ...prevItem, quantity: prevItem.quantity + amount }
+          : prevItem;
+      });
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ allItems, setItems, addToCart, removeFromCart, updateQuantity }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export const useCart = () => {
